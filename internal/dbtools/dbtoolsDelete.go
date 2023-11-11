bf3fbe6c7e814291dc1eb8f8c0860e8c40962597
meatware <114529342+darkpandarts@users.noreply.github.com>
2023-11-11 19:41:21 +0000

    sort out more tests 2

---
+++ internal/dbtools/dbtoolsDelete.go
@@ -0,0 +1,60 @@
+package dbtools
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/stablecaps/services-api-go/pkg/models"
+)
+
+
+func SubmitDeleteRequest(url string, reqBody []byte) models.Service {
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		log.Println("Error creating post request")
+		os.Exit(42)
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error making post request")
+		os.Exit(42)
+	}
+
+	defer resp.Body.Close()
+
+	body, derr := io.ReadAll(resp.Body)
+	if derr != nil {
+		log.Printf("Error decoding post respponse: %s", err)
+		log.Println(resp.Body)
+		os.Exit(42)
+	}
+	fmt.Printf("string body: %s", string(body))
+
+	if resp.StatusCode != http.StatusCreated {
+		log.Printf("Error unexpected status: %d", resp.StatusCode)
+		os.Exit(42)
+	}
+
+	// Unmarshal JSON to Go struct
+	var result models.Service
+	// Parse []byte to go struct pointer
+	if err := json.Unmarshal(body, &result); err != nil {
+		fmt.Println("Can not unmarshal JSON")
+		os.Exit(42)
+	}
+	fmt.Println(PrettyPrint(result))
+
+	fmt.Println("ServiceId:", result.ServiceId)
+	fmt.Println("Name:", result.ServiceName)
+	fmt.Println("Descripton:", result.ServiceDescription)
+
+	return result
+}
