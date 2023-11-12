32d1e2e04f45b2cccb283b460c0d7834f30b34f9
meatware <114529342+darkpandarts@users.noreply.github.com>
2023-11-12 14:09:12 +0000

    fiddle about with db connection options

--- pkg/models/dbConnector.go
+++ pkg/models/dbConnector.go
@@ -20,6 +20,8 @@ type PostgresDb struct {
 	db *sql.DB
 }
 
+// investigate prepared statements - looks like we prob do not need to do this
+// https://www.reddit.com/r/golang/comments/6wll4z/lots_of_prepared_statements_how_do_i_deal_with/
 func NewPostgresDb(userName, dbName, password, sslmode string, maxOpenConns, maxIdleConns int) (*PostgresDb, error) {
 	connStr := fmt.Sprintf("user=%s dbname=%s password=%s sslmode=%s", userName, dbName, password, sslmode)
 	log.Printf("connStr: %s", connStr)
@@ -32,9 +34,10 @@ func NewPostgresDb(userName, dbName, password, sslmode string, maxOpenConns, max
 	}
 
 	// set db connection related settings
+	// https://www.alexedwards.net/blog/configuring-sqldb
 	db.SetMaxOpenConns(maxOpenConns) // Sane default
-	db.SetMaxIdleConns(maxIdleConns)
-	db.SetConnMaxLifetime(time.Nanosecond)
+	db.SetMaxIdleConns(maxIdleConns) // try 2 for some performance gains
+	db.SetConnMaxLifetime(5*time.Minute)
 
 	if err := db.Ping(); err != nil {
 		log.Printf("Error: %s", err)
